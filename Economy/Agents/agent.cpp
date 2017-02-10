9902a4b2bd89b750115f78ffe092d603d13448eb
Clementin Castellano <clem@Clementin>
2017-02-10 01:33:45 +0100

    Changed the layout

--- Economy/Agents/agent.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "agent.h"
-
-Agent::Agent(void)
-{
-  printf("Hello!");
-  printf("\n");
-}
--- Economy/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "Agents/agent.h"
-
-int main()
-{
-  Agent x = Agent();
-  return(0);
-}
